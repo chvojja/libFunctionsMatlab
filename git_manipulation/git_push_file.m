1072e7ec7b2895f73df2a22d174bb7c87560b740
chvojja <chvojja2@gmail.com>
2022-06-28 18:55:27 +0200

    msg

---
+++ git_manipulation/git_push_file.m
@@ -0,0 +1,11 @@
+function git_push_file(nv)
+arguments
+    nv.file_name (1,:) char
+    nv.remote_folder (1,:) char
+end
+
+  sys_commands{1} = ['!git add ' nv.remote_folder '/' nv.file_name '.m' ];
+  system(sys_commands{1});
+
+end
+
